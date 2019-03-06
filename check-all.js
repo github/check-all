cc5edca857b487b99ff269a3e96868c2ab741479
Mu-An Chiou <me@muanchiou.com>
2019-03-06 10:50:21 -0500

    Fix lint

--- check-all.js
+++ check-all.js
@@ -65,7 +65,10 @@ export default function subscribe(container: Element): Subscription {
   function onCheckAllItem(event: Event): void {
     if (event instanceof CustomEvent && event.detail) {
       const {relatedTarget} = event.detail
-      if (relatedTarget && relatedTarget.hasAttribute('data-check-all') || relatedTarget.hasAttribute('data-check-all-item')) {
+      if (
+        relatedTarget &&
+        (relatedTarget.hasAttribute('data-check-all') || relatedTarget.hasAttribute('data-check-all-item'))
+      ) {
         return
       }
     }
