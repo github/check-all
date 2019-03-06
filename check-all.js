722cb16fc2dceb756fc5d174c9f6f79ad891ec1c
慕安 <me@muanchiou.com>
2019-03-06 10:21:02 -0500

    Add condition to onCheckAll

--- check-all.js
+++ check-all.js
@@ -63,9 +63,9 @@ export default function subscribe(container: Element): Subscription {
   }
 
   function onCheckAllItem(event: Event): void {
-    if (event instanceof CustomEvent) {
+    if (event instanceof CustomEvent && event.detail) {
       const {relatedTarget} = event.detail
-      if (relatedTarget.hasAttribute('data-check-all') || relatedTarget.hasAttribute('data-check-all-item')) {
+      if (relatedTarget && relatedTarget.hasAttribute('data-check-all') || relatedTarget.hasAttribute('data-check-all-item')) {
         return
       }
     }
